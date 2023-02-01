03fdc766cc932612fd6ac6efac548b06e7388b1e
RavenChelm <98704006+RavenChelm@users.noreply.github.com>
2023-02-01 21:37:15 +0300

    inventory and another fix SaveLoad
    1)Теперь точно починил SaveLoad
    2)Предметы, добавленные в инвентарь до игры подгружаются
    3)Фикс бага на подбор предмета, когда в инвентаре заранее добавленные предметы дублировались
    4)В inventorySlot изменяется ссылка на объект
    5)Можно брать один предмет из стака правой кнопкой мыши. Но пока нельзя стакать обратно вместе, и с этим связан баг на 26 строке Dropitem

--- Assets/Scripts/Inventory/DragItem.cs
+++ Assets/Scripts/Inventory/DragItem.cs
@@ -10,18 +10,35 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     private CanvasGroup canvasGroup;
     private RectTransform dragLayer;
     private Transform slot;
+    private Inventory inventory;
+    private InventorySlot inventorySlot;
 
     private void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
         dragLayer = GameObject.FindGameObjectWithTag("DragLayer").GetComponent<RectTransform>();
         currentSlot = transform.parent;
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         slot = null;
         dragItem = this;
+        inventorySlot = inventory.GetInventorySlot(transform.parent);
+        if (Input.GetMouseButton(0))
+        {
+            inventorySlot.objectSlot = null;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            if (inventorySlot.amount != 1)
+            {
+                inventorySlot.amount -= 1;
+                inventory.AddItem(inventorySlot.item, inventorySlot.objectSlot, inventorySlot.amount);
+                inventorySlot.amount = 1;
+            }
+        }
         startPosition = transform.position;
         startParrent = transform.parent;
         transform.SetParent(dragLayer);
@@ -39,6 +56,7 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         canvasGroup.blocksRaycasts = true;
         if (slot == null)
         {
+            inventorySlot.objectSlot = startParrent; ////
             transform.SetParent(startParrent);
             transform.position = startPosition;
         }
@@ -48,6 +66,7 @@ public class DragItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     public void SetItemToSlot(Transform slot)
     {
         this.slot = slot;
+        inventorySlot.objectSlot = slot;     ////
         transform.SetParent(slot);
         currentSlot = slot;
         transform.localPosition = Vector3.zero;
--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -8,10 +8,13 @@ public class InventorySlot
 {
     public Item item;
     public int amount;
-    public InventorySlot(Item item, int amount = 1)
+    public Transform objectSlot;
+    public InventorySlot(Item item, Transform objectSlot, int amount = 1)
     {
         this.item = item;
+        this.objectSlot = objectSlot;
         this.amount = amount;
+
     }
 }
 public class Inventory : MonoBehaviour, IAddItem
@@ -19,8 +22,8 @@ public class Inventory : MonoBehaviour, IAddItem
     [SerializeField] private GameObject InventoryPanel;
     [SerializeField] private GameObject iconPrefab;
     [SerializeField] private List<InventorySlot> items = new List<InventorySlot>();
-    [SerializeField] private List<Transform> slots = new List<Transform>();
-    [SerializeField] private int size;
+    private List<Transform> slots = new List<Transform>();
+    private int size;
     [SerializeField] private int steckCount = 3;
     private void Start()
     {
@@ -30,7 +33,22 @@ public class Inventory : MonoBehaviour, IAddItem
             slots.Add(InventoryPanel.transform.GetChild(i));
         }
 
+
+        foreach (InventorySlot inventorySlot in items)
+        {
+            iconPrefab.GetComponent<Image>().sprite = inventorySlot.item.icon;
+            foreach (Transform slot in slots)
+            {
+                if (slot.childCount == 0)
+                {
+                    Instantiate(iconPrefab, slot);
+                    inventorySlot.objectSlot = slot;
+                    break;
+                }
+            }
+        }
     }
+
     public void AddItem(Item item, int amount = 1)
     {
         foreach (InventorySlot slot in items)
@@ -43,11 +61,60 @@ public class Inventory : MonoBehaviour, IAddItem
         }
 
         if (items.Count >= size) return;
-        InventorySlot new_slot = new InventorySlot(item, amount);
+
+        iconPrefab.GetComponent<Image>().sprite = item.icon;
+        foreach (Transform slot in slots)
+        {
+            if (slot.childCount == 0)
+            {
+                Instantiate(iconPrefab, slot);
+                InventorySlot new_slot = new InventorySlot(item, slot, amount);
+                items.Add(new_slot);
+                break;
+            }
+        }
+
+    }
+    public void AddItem(Item item, Transform slotTrn, int amount = 1)
+    {
+
+        iconPrefab.GetComponent<Image>().sprite = item.icon;
+        Instantiate(iconPrefab, slotTrn);
+        InventorySlot new_slot = new InventorySlot(item, slotTrn, amount);
         items.Add(new_slot);
-        UpdateUI();
     }
 
+
+    public void RemoveItem(InventorySlot inventorySlot, int amount = 1)
+    {
+        foreach (InventorySlot slot in items)
+        {
+            if (slot.item.id == inventorySlot.item.id)
+            {
+                if (slot.amount > 1) { slot.amount -= 1; break; }
+                else
+                {
+                    Destroy(inventorySlot.objectSlot.gameObject);
+                    items.Remove(inventorySlot);
+                    break;
+                }
+            }
+        }
+    }
+    public void SwapItem(InventorySlot itemOne, InventorySlot itemTwo)
+    {
+        Transform tmp = itemOne.objectSlot;
+        itemOne.objectSlot = itemTwo.objectSlot;
+        itemTwo.objectSlot = tmp;
+    }
+    public InventorySlot GetInventorySlot(Transform trn)
+    {
+        foreach (InventorySlot item in items)
+        {
+            if (item.objectSlot == trn) { return item; }
+        }
+        return null;
+    }
     public Item GetItem(int i)
     {
         return i < items.Count ? items[i].item : null;
@@ -60,15 +127,7 @@ public class Inventory : MonoBehaviour, IAddItem
     {
         return items.Count;
     }
-    private void UpdateUI()
-    {
-        for (int i = 0; i < GetSize() && i < slots.Count; i++)
-        {
-            iconPrefab.GetComponent<Image>().sprite = GetItem(i).icon;
-            if (slots[i].childCount == 0)
-                Instantiate(iconPrefab, slots[i]);
-        }
-    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
@@ -84,4 +143,23 @@ public class Inventory : MonoBehaviour, IAddItem
     {
         EventBus.Unsubscribe(this);
     }
+
+    // public void RemoveItem(Item item, int amount = 1)
+    // {
+    //     InventorySlot toDelete = null;
+    //     foreach (InventorySlot slot in items)
+    //     {
+    //         if (slot.item.id == item.id)
+    //         {
+    //             if (slot.amount > 1) { slot.amount -= 1; break; }
+    //             else { toDelete = slot; break; }
+    //         }
+    //     }
+    //     if (toDelete != null)
+    //     {
+    //         Destroy(toDelete.objectSlot.gameObject);
+    //         items.Remove(toDelete);
+    //     }
+    // }
+
 }
