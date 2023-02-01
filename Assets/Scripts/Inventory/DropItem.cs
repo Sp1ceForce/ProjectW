e9543854617d2d4df424f4e6998f7105704a34b8
RavenChelm <98704006+RavenChelm@users.noreply.github.com>
2023-02-01 21:37:15 +0300

    inventory and another fix SaveLoad
    1)Теперь точно починил SaveLoad
    2)Предметы, добавленные в инвентарь до игры подгружаются
    3)Фикс бага на подбор предмета, когда в инвентаре заранее добавленные предметы дублировались
    4)В inventorySlot изменяется ссылка на объект
    5)Можно брать один предмет из стака правой кнопкой мыши. Но пока нельзя стакать обратно вместе, и с этим связан баг на 26 строке Dropitem

--- Assets/Scripts/Inventory/DropItem.cs
+++ Assets/Scripts/Inventory/DropItem.cs
@@ -3,6 +3,12 @@ using UnityEngine.EventSystems;
 
 public class DropItem : MonoBehaviour, IDropHandler
 {
+    private Inventory inventory;
+    private InventorySlot inventorySlot;
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
     public void OnDrop(PointerEventData eventData)
     {
         var item = DragItem.dragItem;
@@ -14,6 +20,19 @@ public class DropItem : MonoBehaviour, IDropHandler
         }
         else if (item != null && childrens.Length > 0)
         {
+            InventorySlot inventorySlotFromDragItem = inventory.GetInventorySlot(item.transform);
+            InventorySlot inventorySlotFromDrop = inventory.GetInventorySlot(transform);
+
+            if (inventorySlotFromDragItem.item.id == inventorySlotFromDrop.item.id)
+            {
+                //Логика объединения
+                //Если итоговый amount <=3
+                //inventorySlotFromDrop.amount + inventorySlotFromDragItem.amount
+                //iventory.remove(inventorySlotFromDragItem)
+                //Если итоговый amount > 3
+                //добавить в слот до трёх
+                //скинуть обратно остатки
+            }
             var slot = item.currentSlot;
             childrens[0].SetItemToSlot(slot);
             item.SetItemToSlot(transform);
