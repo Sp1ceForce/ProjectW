3903c4af4caff9035ffe35bc639d5f8ee62389d0
R_aven <98704006+RavenChelm@users.noreply.github.com>
2023-02-11 15:52:21 +0300

    Revert "try add Eug branch"

--- Assets/Scripts/EnemyScripts/EnemyKnockbackComponent.cs
+++ Assets/Scripts/EnemyScripts/EnemyKnockbackComponent.cs
@@ -4,10 +4,10 @@ using UnityEngine.AI;
 public class EnemyKnockbackComponent : MonoBehaviour
 {
     float knockedTime = 0.3f;
-    public void KnockBack(Vector3 instigatorPosition, float knockbackForce){
+    public void KnockBack(Transform Instigator, float knockbackForce){
         GetComponent<NavMeshAgent>().enabled = false;
         GetComponent<EnemyStateController>().enabled = false;
-        Vector3 knockdirection = transform.position - instigatorPosition;
+        Vector3 knockdirection = transform.position - Instigator.transform.position;
         GetComponent<Rigidbody>().AddForce(knockdirection.normalized * knockbackForce,ForceMode.Impulse);
         StartCoroutine(KnockedCooldown());
     }
