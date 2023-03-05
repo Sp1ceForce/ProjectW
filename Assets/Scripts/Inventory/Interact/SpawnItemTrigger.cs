bf2e6a10121b9c80ae4a3160c3b741248e0243ce
malyashuk <malyashuk@mail.ru>
2023-03-05 14:14:55 +0300

    Add SpawnItemHandler with SpanItemTrigger

---
+++ Assets/Scripts/Inventory/Interact/SpawnItemTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItemTrigger : AfterInteract
+{
+
+    //[SerializeField] private GameObject trigger;
+    public GameObject fieldsForSpawn;
+    public GameObject SpawnedItem;
+    public int amountItems = 1;
+    public float sizeField = 5f;
+    private SpawnItemHandler spawnItemHandler = new SpawnItemHandler();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnItemHandler.CreateListOfFields(fieldsForSpawn);
+        spawnItemHandler.SetSizeField(sizeField);
+    }
+
+    public override void AfterInteractLogic()
+    {
+        spawnItemHandler.SpawnItemsInRandomPalaceAndField(SpawnedItem, amountItems);
+    }
+
+}
+
